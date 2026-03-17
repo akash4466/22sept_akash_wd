4e966b3b316e1e529f34b2d9b5e82967bfc375a2
akash4466 <polakash918@gmail.com>
2026-03-17 12:53:31 +0530

    commite

---
+++ topsf1/assingment/python/main11.py
@@ -0,0 +1,6 @@
+list1 = ['apple', 'banana', 'mango']
+
+for item in list1:
+    if item == 'banana':
+        continue
+    print(item)
---
+++ topsf1/assingment/python/main2.py
@@ -0,0 +1,8 @@
+# Program to demonstrate variables, comments, and indentation
+
+name = "Akash"
+age = 20
+
+# printing values
+print("Name:", name)
+print("Age:", age)
